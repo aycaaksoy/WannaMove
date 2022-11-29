f436199490cf62c05d4910b28283472567615ae6
Ayça Aksoy <aycaaksoy2000@gmail.com>
2022-11-29 17:08:55 +0300

    Add project files.

---
+++ WannaMove/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WannaMove.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

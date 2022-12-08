6519da7f24ab5bef1036b3dac255e14b767fb24c
Ayça Aksoy <aycaaksoy2000@gmail.com>
2022-12-08 16:48:18 +0300

    layout cleaning

---
+++ WannaMove/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WannaMove.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult AboutTeam()
+        {
+            return View();
+        }
+    }
+}

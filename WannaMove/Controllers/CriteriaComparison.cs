e66e153766df0c9e0f17f29472566f994d8a6343
Ayça Aksoy <aycaaksoy2000@gmail.com>
2022-12-10 19:39:11 +0300

    extra stuff

---
+++ WannaMove/Controllers/CriteriaComparison.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WannaMove.Controllers
+{
+    public class CriteriaComparison : Controller
+    {
+        public IActionResult PairwiseComparison()
+        {
+            return View();
+        }
+    }
+}

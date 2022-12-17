82a3d7f12168d9dd08fc5c3b551322b5cfd428d1
Ayça Aksoy <aycaaksoy2000@gmail.com>
2022-12-17 18:15:36 +0300

    atakanın eklemeleri

---
+++ WannaMove/Controllers/DashboardsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WannaMove.Controllers
+{
+    public class DashboardsController : Controller
+    {
+        public IActionResult Dashboards()
+        {
+            return View();
+        }
+    }
+}
